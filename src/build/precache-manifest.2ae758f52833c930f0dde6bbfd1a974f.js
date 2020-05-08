dcd0965b63c81cb22445af6f7e38f3eb23ddcb5d
Kioumars Ghereghlou <minimac@WorkStation.local>
2020-05-08 12:24:55 -0400

    First commit

---
+++ src/build/precache-manifest.2ae758f52833c930f0dde6bbfd1a974f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687a9315200bf0706041fa05e2f164eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d379d28478e33f51368e",
+    "url": "/static/css/main.ed56decf.chunk.css"
+  },
+  {
+    "revision": "6ac32d2e9b58823ea516",
+    "url": "/static/js/2.aa04e143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa04e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d379d28478e33f51368e",
+    "url": "/static/js/main.9d021c0e.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
